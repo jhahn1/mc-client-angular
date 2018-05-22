75a066e021b841ef1aa85988e3d790ffbab30318
Jeremy Hahn <jeremydhahn@gmail.com>
2018-05-22 16:53:23 -0400

    WIP

--- src/app/challenge/challenge.component.ts
+++ src/app/challenge/challenge.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 import { AuthService } from '../auth/auth.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Component({
   selector: 'app-challenge',
@@ -15,7 +23,8 @@ export class ChallengeComponent implements OnInit {
   secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) {
+  constructor(private _formBuilder: FormBuilder,
+    private http: HttpClient) {
    }
 
   ngOnInit() {
@@ -32,6 +41,24 @@ export class ChallengeComponent implements OnInit {
 
   submitChallenge(): void {
     console.log('Values = ', this.firstFormGroup.value, this.secondFormGroup.value, this.thirdFormGroup.value);
+    this.http.post('localhost:8080/challenge/create', 'nonsense', httpOptions).pipe(
+      tap(_ => console.log(`shit happened`)),
+      catchError(this.handleError(`somethings super wrong`))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
 }
